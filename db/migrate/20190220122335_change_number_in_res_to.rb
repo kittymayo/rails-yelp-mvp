ac3fe5a1359b3a322e45e4dc66ed65e6949c85dd
Kitty Mayo <kitty.m.mayo@gmail.com>
2019-02-20 12:26:05 +0000

    changed column to phone number

---
+++ db/migrate/20190220122335_change_number_in_res_to.rb
@@ -0,0 +1,4 @@
+class ChangeNumberInResTo < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_120313) do
+ActiveRecord::Schema.define(version: 2019_02_20_122335) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_02_20_120313) do
     t.integer "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
